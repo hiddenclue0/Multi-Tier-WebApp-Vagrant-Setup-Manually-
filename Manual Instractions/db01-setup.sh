8b202b706282d81f443b687e39ab7a5ef44b254e
jakir <hiddenclue0@gmail.com>
2025-06-19 18:36:44 +0600

    vagrant up

--- Manual Instractions/db01-setup.sh
+++ Manual Instractions/db01-setup.sh
@@ -5,41 +5,44 @@ sudo dnf update -y
 sudo dnf install epel-release -y
 sudo dnf install git mariadb-server -y
 
-# Start and enable MariaDB
+# Start and enable and status check MariaDB
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
+sudo systemctl status  mariadb
 
 # Secure MariaDB Installation
-echo "Securing MariaDB..."
-sudo mysql -u root <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'admin123';
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%';
-FLUSH PRIVILEGES;
-EOF
+sudo mysql_secure_installation
+# Switch to unix_socket authentication [Y/n] Y
+# Change the root password? [Y/n] Y
+# New password: admin123
+# Re-enter new password: admin123
+# Remove anonymous users? [Y/n] Y
+# Disallow root login remotely? [Y/n] Y
+# Remove test database and access to it? [Y/n] Y
+# Reload privilege tables now? [Y/n] Y
 
 # Create database and user
-sudo mysql -u root -padmin123 <<EOF
-CREATE DATABASE accounts;
-GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'localhost' IDENTIFIED BY 'admin123';
-GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'%' IDENTIFIED BY 'admin123';
-FLUSH PRIVILEGES;
-EOF
+sudo mysql -u root -padmin123 
+MariaDB [(none)]> CREATE DATABASE accounts;
+MariaDB [(none)]> GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'localhost' IDENTIFIED BY 'admin123';
+MariaDB [(none)]> GRANT ALL PRIVILEGES ON accounts.* TO 'admin'@'%' IDENTIFIED BY 'admin123';
+MariaDB [(none)]> FLUSH PRIVILEGES;
+MariaDB [(none)]> exit;
 
 # Clone project and import DB
 cd /tmp/
-git clone -b local https://github.com/hkhcoder/vprofile-project.git
-cd vprofile-project
+git clone https://github.com/hiddenclue0/spring-multi-tier-enterprise-app.git
+cd spring-multi-tier-enterprise-app
 mysql -u root -padmin123 accounts < src/main/resources/db_backup.sql
 
-# Firewall settings
-sudo systemctl start firewalld
-sudo systemctl enable firewalld
-sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
-sudo firewall-cmd --reload
-
-# Restart MariaDB to apply everything
+# Restart MariaDB and status check
 sudo systemctl restart mariadb
+sudo systemctl status  mariadb
 
-echo "✅ db01 setup complete."
+#Show databases
+mysql -u root -padmin123 -e accounts
+MariaDB [accounts]> show tables;
+MariaDB [accounts]> exit;
+
+# Logout db01
+exit
